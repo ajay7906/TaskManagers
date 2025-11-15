d2764346ea3f3a30ade50026f1546ceb9e5c3776
upsaindiatech-pvt-nology <upsaindiatech@gmail.com>
2025-11-15 11:32:51 +0530

    completed

--- doc/swagger.js
+++ doc/swagger.js
@@ -1,29 +1,3 @@
-// const swaggerJSDoc = require("swagger-jsdoc");
-
-// const options = {
-//   definition: {
-//     openapi: "3.0.0",
-//     info: {
-//       title: "Task Manager API",
-//       version: "1.0.0",
-//       description: "Backend API for task management assignment"
-//     },
-//     servers: [
-//       { url: "http://localhost:4000", description: "Local server" }
-//     ],
-//     components: {
-//       securitySchemes: {
-//         bearerAuth: { type: "http", scheme: "bearer", bearerFormat: "JWT" }
-//       }
-//     },
-//     security: [{ bearerAuth: [] }]
-//   },
-//   apis: [] // We won't scan files automatically; you can add JSDoc comments later.
-// };
-
-// const swaggerSpec = swaggerJSDoc(options);
-// module.exports = swaggerSpec;
-
 
 
 // src/docs/swagger.js
